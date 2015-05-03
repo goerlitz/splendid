268ac39867dec571bc65327ced1cb35bdc3949af
olaf.goerlitz <olaf.goerlitz@gmail.com>
2015-05-04 00:57:39 +0200

    added async execution for remote SPARQL query

--- src/main/scala/splendid/execution/RemoteQuery.scala
+++ src/main/scala/splendid/execution/RemoteQuery.scala
@@ -1,52 +1,90 @@
 package splendid.execution
 
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import scala.concurrent.Promise
+import scala.util.Failure
+import scala.util.Success
+
 import org.openrdf.query.QueryLanguage
+import org.openrdf.query.TupleQueryResult
+import org.openrdf.repository.RepositoryException
 import org.openrdf.repository.sparql.SPARQLRepository
 
 import akka.actor.Actor
 import akka.actor.ActorLogging
+import akka.pattern.pipe
 import splendid.Done
 import splendid.Result
 
 case class SparqlQuery(endpoint: String, query: String)
 
-class RemoteQuery extends Actor with ActorLogging {
+case class SparqlTupleResult(client: SparqlEndpointClient, result: TupleQueryResult)
 
-  def receive = {
-    case SparqlQuery(endpoint, query) => {
+trait SparqlEndpointClient {
+  def evalTupleQuery(query: String)(implicit exec: ExecutionContext): Future[SparqlTupleResult]
+  def close(): Unit
+}
 
-      // TODO: make asynchronous
-//      log.info(s"executing query '$query' at $endpoint")
+// TODO check if the HTTP client can be reused for consecutive queries to the same SPARQL endpoint
+class EndpointClient(uri: String) extends SparqlEndpointClient {
 
-      // prepare connection to SPARQL endpoint
-      val repo = new SPARQLRepository(endpoint);
+  val repo = new SPARQLRepository(uri);
 
-      // TODO: handle exception
+  override def evalTupleQuery(query: String)(implicit exec: ExecutionContext): Future[SparqlTupleResult] = {
+
+    val promise = Promise[SparqlTupleResult]
+
+    // async tuple query evaluation
+    Future {
       try {
         repo.initialize()
         val con = repo.getConnection()
 
         try {
-          val tq = con.prepareTupleQuery(QueryLanguage.SPARQL, query, null)
-
-          // TODO: handle exceptions
-          val result = tq.evaluate();
-          while (result.hasNext()) {
-            context.parent ! Result(result.next())
-          }
-          result.close()
+          // TODO check if use result handler gives better performance
+          val result = con.prepareTupleQuery(QueryLanguage.SPARQL, query, null).evaluate()
+          promise.success(SparqlTupleResult(this, result))
         } catch {
-          case e: Throwable => // TODO: handle exceptions
-        } finally {
-          con.close()
+          case t: Throwable =>
+            con.close()
+            repo.shutDown()
+            promise.failure(t)
         }
+
       } catch {
-        case e: Throwable => // TODO: handle exceptions
-      } finally {
-        repo.shutDown()
+        case re: RepositoryException =>
+          repo.shutDown()
+          promise.failure(re)
       }
-      context.parent ! Done // always send DONE?
     }
+
+    promise.future
+  }
+
+  override def close(): Unit = repo.shutDown()
+}
+
+object EndpointClient {
+  def apply(uri: String): EndpointClient = new EndpointClient(uri)
+}
+
+// TODO add SPARQL endpoint client as parameter to allow for better unit testing
+class RemoteQuery extends Actor with ActorLogging {
+
+  implicit val exec = context.dispatcher
+
+  def receive = {
+    case SparqlQuery(uri, query) => EndpointClient(uri) evalTupleQuery query pipeTo self
+    case SparqlTupleResult(client, result) =>
+      while (result.hasNext()) {
+        context.parent ! Result(result.next())
+      }
+      result.close()
+      client.close()
+      context.parent ! Done
+    case Failure(err) => println("err: {}", err) // TODO propagate error
     case _ => // TODO ignore or send error message
   }
+
 }
